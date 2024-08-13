27b0de1e46ccb85c6346f23034dbf0087f20ae60
Eriola <endoj22@gmail.com>
2024-08-14 01:16:39 +0200

    toggle icon fix

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.toggle(darkThemeClass);
         
         if (document.body.classList.contains(darkThemeClass)) {
-            themeIcon.classList.replace('fa-sun', 'fa-moon');
-        } else {
             themeIcon.classList.replace('fa-moon', 'fa-sun');
+        } else {
+            themeIcon.classList.replace('fa-sun', 'fa-moon');
         }
     });
 });
