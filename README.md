8e987ddf43164186ebf98ad70d9c5b7d4777a7c1
Eriola <endoj22@gmail.com>
2024-08-13 22:43:14 +0200

    implementing dark mode functionality in portfolio website

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A static website clone of Python.org using HTML and CSS only.
+Dark/Light theme.
---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
+    const darkThemeClass = 'dark-theme';
+
+    themeToggleBtn.addEventListener('click', () => {
+        document.body.classList.toggle(darkThemeClass);
+        
+        if (document.body.classList.contains(darkThemeClass)) {
+            themeIcon.classList.replace('fa-sun', 'fa-moon');
+        } else {
+            themeIcon.classList.replace('fa-moon', 'fa-sun');
+        }
+    });
+});
